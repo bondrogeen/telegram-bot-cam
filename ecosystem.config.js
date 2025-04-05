327ed24b0ecca76e11e3ea90bb5c3592bc617691
Roman Bondarev <bondrogeen@gmail.com>
2025-04-05 22:09:20 +0300

    ddd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       // port: 3001,
       exec_mode: "cluster",
       instances: 1,
-      script: "./build/app.js",
+      script: "node ./build/app.js",
       args: "",
       // node_args : '-r dotenv/config',
     },
--- src/app.js
+++ src/app.js
@@ -21,58 +21,59 @@ const store = new Store(db, { users: [admin], cameras: [], })
 console.log(telegram);
 
 
-// bot.use(async (ctx, next) => {
-// 	// console.log(ctx);
-// 	if (typeof ctx.user === 'undefined') {
-// 		ctx.user = store.find('users', ctx);
-// 		ctx.store = store;
-// 		ctx.visitors = [];
-// 		ctx.$t = (value) => $t(value, ctx?.user?.locale);
-// 	}
-// 	if (ctx?.user?.isAccess?.()) {
-// 		const path = ctx?.update?.callback_query?.data;
-// 		if (path && path.includes('admin')) {
-// 			if (ctx.user.isAdmin()) {
-// 				await next();
-// 			}
-// 		} else {
-// 			await next();
-// 		}
-// 		return;
-// 	} else {
-// 		const { id, first_name, username } = ctx?.message?.from || {};
-// 		console.log(ctx?.message?.from);
-// 		if (!visitorsIds?.[id]) {
-// 			visitorsIds[id] = true;
-// 			ctx.visitors.push({ id, first_name, username });
-// 		}
-// 		ctx.reply(`Hello ${first_name || username || ''}`);
-// 	}
-// });
+bot.use(async (ctx, next) => {
+	// console.log(ctx);
+	if (typeof ctx.user === 'undefined') {
+		ctx.user = store.find('users', ctx);
+		ctx.store = store;
+		ctx.visitors = [];
+		ctx.$t = (value) => $t(value, ctx?.user?.locale);
+	}
+	if (ctx?.user?.isAccess?.()) {
+		const path = ctx?.update?.callback_query?.data;
+		if (path && path.includes('admin')) {
+			if (ctx.user.isAdmin()) {
+				await next();
+			}
+		} else {
+			await next();
+		}
+		return;
+	} else {
+		const { id, first_name, username } = ctx?.message?.from || {};
+		console.log(ctx?.message?.from);
+		if (!visitorsIds?.[id]) {
+			visitorsIds[id] = true;
+			ctx.visitors.push({ id, first_name, username });
+		}
+		ctx.reply(`Hello ${first_name || username || ''}`);
+	}
+});
 
 
-// bot.on('callback_query:data', async (ctx, next) => {
-// 	console.log(
-// 		'another callbackQuery happened',
-// 		ctx.callbackQuery.data.length,
-// 		ctx.callbackQuery.data
-// 	);
-// 	return next();
-// });
+bot.on('callback_query:data', async (ctx, next) => {
+	console.log(
+		'another callbackQuery happened',
+		ctx.callbackQuery.data.length,
+		ctx.callbackQuery.data
+	);
+	return next();
+});
 
-// bot.command('start', async (ctx) => menus.replyToContext(ctx));
-// bot.use(menus.middleware());
+bot.command('start', async (ctx) => menus.replyToContext(ctx));
+bot.use(menus.middleware());
 
-// bot.catch((error) => {
-// 	console.log('bot error', error);
-// });
+bot.catch((error) => {
+	console.log('bot error', error);
+});
 
 async function start() {
 	try {
+		await ftp.listen();
+		await bot.launch();
+
 		console.log(11111);
-		
-		// await ftp.listen();
-		// await bot.launch();
+
 	} catch (error) {
 		console.log(error);
 	}
